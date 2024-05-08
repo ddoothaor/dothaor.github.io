bf474c9160575d9eac95d12fb092aea74c991b57
ddoothaor <160983845+ddoothaor@users.noreply.github.com>
2024-05-09 00:18:03 +0700

    Delete thucdon.js

--- thucdon.js
+++
@@ -1,6 +0,0 @@
-let slider = document.querySelector('.item');
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let dots = document.querySelectorAll('.slider .dots li');
-
